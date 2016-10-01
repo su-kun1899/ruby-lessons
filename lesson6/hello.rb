6d154b6c32bcf650e6e5bf86390948a41ed6e013
su-kun1899 <higedrum.coz@gmail.com>
2016-10-02 04:26:28 +0900

    Use string object

---
+++ lesson6/hello.rb
@@ -0,0 +1,18 @@
+# 文字列
+
+# 特殊文字
+puts "hell\no worl\td"
+puts 'hell\no worl\td'
+
+# 式展開
+puts "price #{3000 * 4}"
+puts 'price #{3000 * 4}'
+
+name = "koji"
+p name.class
+puts "hello #{name}"
+
+# 文字列連結
+puts "hello " + "world"
+# 繰り返し
+puts "hello " * 10
