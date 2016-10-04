faacd8221958128f71f664ddce717ded602982cd
su-kun1899 <higedrum.coz@gmail.com>
2016-10-04 22:21:32 +0900

    Method with ! and ?

---
+++ lesson7/hello.rb
@@ -0,0 +1,13 @@
+# upcase! 破壊的なメソッド
+# downcase reverse など
+
+name = "sudo"
+puts name.upcase
+puts name # sudo
+puts name.upcase!
+puts name # SUDO
+
+# ? 真偽値 true false
+
+p name.empty?
+p name.include?("S")
